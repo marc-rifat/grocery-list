4dc4fa59362117b62c97e695039e823e64acdd8a
marc-rifat <79622206+marc-rifat@users.noreply.github.com>
2025-01-22 13:39:57 -0500

    wrapped user entity with lombok, also added constructor to user entity

--- src/main/java/com/example/springboot/grocerylist/entity/User.java
+++ src/main/java/com/example/springboot/grocerylist/entity/User.java
@@ -1,9 +1,15 @@
 package com.example.springboot.grocerylist.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,46 +23,9 @@ public class User {
 
     private boolean isAdmin;
 
-    // Default constructor
-    public User() {}
-
-    // Constructor with fields
     public User(String username, String password, boolean isAdmin) {
         this.username = username;
         this.password = password;
         this.isAdmin = isAdmin;
-    }
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public boolean isAdmin() {
-        return isAdmin;
-    }
-
-    public void setAdmin(boolean admin) {
-        isAdmin = admin;
     }
 } 
